e2855aab51efe2b6de659976ecdc45f3f2590dd1
Juli Mallett <juli@redactedconsulting.com>
2006-01-29 09:26:50 +0000

    Shuffle .PATH entries.

--- mu/mk/build/build.mk
+++ mu/mk/build/build.mk
@@ -1,4 +1,7 @@
-# $Id: build.mk,v 1.1 2006-01-29 07:01:57 juli Exp $
+# $Id: build.mk,v 1.2 2006-01-29 09:26:50 juli Exp $
+
+.PATH: ${KERNEL_ROOT}/core
+.PATH: ${KERNEL_ROOT}/io/device/console
 
 KERNEL_SOURCES+=console.c
 KERNEL_SOURCES+=framebuffer.c

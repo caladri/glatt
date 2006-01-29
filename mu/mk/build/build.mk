d45ca24c80a90dad3ce6fe6df3b328cb4c74ec48
Juli Mallett <juli@redactedconsulting.com>
2006-01-29 23:56:08 +0000

    Repo-copy some files to have less ambiguous names.

--- mu/mk/build/build.mk
+++ mu/mk/build/build.mk
@@ -1,12 +1,12 @@
-# $Id: build.mk,v 1.5 2006-01-29 23:52:26 juli Exp $
+# $Id: build.mk,v 1.6 2006-01-29 23:56:06 juli Exp $
 
 .PATH: ${KERNEL_ROOT}/core
 .PATH: ${KERNEL_ROOT}/io/device/console
 .PATH: ${KERNEL_ROOT}/vm
 
 # core
-KERNEL_SOURCES+=mp.c
-KERNEL_SOURCES+=spinlock.c
+KERNEL_SOURCES+=core_mp.c
+KERNEL_SOURCES+=core_spinlock.c
 
 # io/devices/console
 KERNEL_SOURCES+=console.c
@@ -14,5 +14,5 @@ KERNEL_SOURCES+=framebuffer.c
 KERNEL_SOURCES+=framebuffer_font.c
 
 # vm
-KERNEL_SOURCES+=page.c
+KERNEL_SOURCES+=vm_page.c
 KERNEL_SOURCES+=vm.c

2389261a3c67e0a64cd73f55caa23c44658da713
Juli Mallett <juli@redactedconsulting.com>
2006-01-29 10:17:16 +0000

    C99-style bool.

--- mu/mk/core/types.h
+++ mu/mk/core/types.h
@@ -4,7 +4,16 @@
 #include <core/_stdarg.h>
 #include <cpu/types.h>
 
+	/* Size types and friends.  */
+
 typedef	uint64_t	size_t;
 typedef	int64_t		ssize_t;
+
+	/* C99 boolean support.  */
+
+typedef	enum _bool {
+	false =		0 != 0,
+	true =		1 == 1,
+} bool;
 
 #endif /* !_CORE_TYPES_H_ */

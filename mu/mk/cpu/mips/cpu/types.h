9f170254f6c05dd91b5bcd3ce61df9529e55cda4
Juli Mallett <juli@redactedconsulting.com>
2006-01-29 03:42:33 +0000

    Add <cpu/types.h>

---
+++ mu/mk/cpu/mips/cpu/types.h
@@ -0,0 +1,20 @@
+#ifndef	_CPU_TYPES_H_
+#define	_CPU_TYPES_H_
+
+	/* C99 integer types with bit-width specification.  */
+
+typedef	signed char		int8_t;
+typedef	unsigned char		uint8_t;
+typedef	signed short		int16_t;
+typedef	unsigned short		uint16_t;
+typedef	signed int		int32_t;
+typedef	unsigned int		uint32_t;
+typedef	signed long		int64_t;
+typedef	unsigned long		uint64_t;
+
+	/* Pointer-related integer types.  */
+
+typedef	int64_t			intptr_t;
+typedef	uint64_t		uintptr_t;
+
+#endif /* !_CPU_TYPES_H_ */

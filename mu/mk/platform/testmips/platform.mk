68c20248e0f4c7f85552ebb5f2410a6c0da2c956
Juli Mallett <juli@redactedconsulting.com>
2006-01-29 23:57:09 +0000

    Repo-copy startup.c to platform_start.c

--- mu/mk/platform/testmips/platform.mk
+++ mu/mk/platform/testmips/platform.mk
@@ -1,10 +1,10 @@
-# $Id: platform.mk,v 1.4 2006-01-29 22:20:17 juli Exp $
+# $Id: platform.mk,v 1.5 2006-01-29 23:57:09 juli Exp $
 
 .PATH: ${PLATFORM_ROOT}/platform
 .PATH: ${PLATFORM_ROOT}/device
 
 KERNEL_SOURCES+=platform_mp.c
-KERNEL_SOURCES+=startup.c
+KERNEL_SOURCES+=platform_start.c
 
 CPU=		mips
 

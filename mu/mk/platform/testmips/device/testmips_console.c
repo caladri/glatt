750bc2a52842f5578269808228238b13efe2049f
Juli Mallett <juli@redactedconsulting.com>
2006-01-30 09:16:44 +0000

    Leave a 1MB hole below the kernel.

--- mu/mk/platform/testmips/device/testmips_console.c
+++ mu/mk/platform/testmips/device/testmips_console.c
@@ -82,16 +82,21 @@ platform_start(void)
 
 	/* 
 	 * Add all global memory.  Processor-local memory will be added by
-	 * the processor that owns it.  We skip the first 4MB of physical
+	 * the processor that owns it.  We skip the first 5MB of physical
 	 * RAM because that's where the kernel will be loaded.  If we start
-	 * to need more than 4MB, we're screwed.
+	 * to need more than 4MB, we're screwed.  Note that we have to load
+	 * the kernel at 1MB to leave room for exception vectors and such
+	 * at the start of physically-addressable memory.
 	 */
 #define	KERNEL_MAX_SIZE		(4 * 1024 * 1024)
-	if (membytes <= KERNEL_MAX_SIZE)
+#define	KERNEL_OFFSET		(1 * 1024 * 1024)
+#define	KERNEL_PHYSICAL_HOLE	(KERNEL_MAX_SIZE + KERNEL_OFFSET)
+	if (membytes <= KERNEL_PHYSICAL_HOLE)
 		panic("%s: not enough attached memory.");
-	membytes -= KERNEL_MAX_SIZE;
-	error = page_insert_pages(KERNEL_MAX_SIZE, PA_TO_PAGE(membytes));
+	membytes -= KERNEL_PHYSICAL_HOLE;
+	error = page_insert_pages(KERNEL_PHYSICAL_HOLE, PA_TO_PAGE(membytes));
 	if (error != 0)
 		panic("page_insert_pages %lu..%lu failed: %d",
-		      PA_TO_PAGE(KERNEL_MAX_SIZE), PA_TO_PAGE(membytes), error);
+		      PA_TO_PAGE(KERNEL_PHYSICAL_HOLE),
+		      PA_TO_PAGE(membytes), error);
 }

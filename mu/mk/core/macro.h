1d5ab30d0792c77cec41ead30dd42b33cbeabe49
Juli Mallett <juli@redactedconsulting.com>
2006-01-30 01:42:45 +0000

    Add MIN and MAX.

--- mu/mk/core/macro.h
+++ mu/mk/core/macro.h
@@ -1,6 +1,9 @@
 #ifndef	_CORE_MACRO_H_
 #define	_CORE_MACRO_H_
 
+#define	MIN(a, b)		((a) < (b) ? (a) : (b))
+#define	MAX(a, b)		((a) > (b) ? (a) : (b))
+
 #define	_CONCAT(x, y)		x ## y
 #define	CONCAT(x, y)		_CONCAT(x, y)
 #define	STRING(t)		#t

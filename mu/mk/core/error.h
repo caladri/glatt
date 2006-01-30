90b8302113805d3abe32f5576ae8c31bfc8505aa
Juli Mallett <juli@redactedconsulting.com>
2006-01-30 00:26:50 +0000

    Add <core/error.h>

---
+++ mu/mk/core/error.h
@@ -0,0 +1,9 @@
+#ifndef	_CORE_ERROR_H_
+#define	_CORE_ERROR_H_
+
+#define	ERROR_IT_S_ALRIGHT	(0x0000)	/* Sterling Void.  */
+#define	ERROR_NOT_FOUND		(0x0001)	/* Resource not found.  */
+#define	ERROR_NOT_PERMITTED	(0x0002)	/* Operation not permitted.  */
+#define	ERROR_EXHAUSTED		(0x0003)	/* Resource exhausted.  */
+
+#endif /* !_CORE_ERROR_H_ */

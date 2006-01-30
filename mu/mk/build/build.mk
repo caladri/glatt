b7b8819e2794f738ba85e8869d273324b8d6a38f
Juli Mallett <juli@redactedconsulting.com>
2006-01-30 00:10:11 +0000

    Search the debugger.

--- mu/mk/build/build.mk
+++ mu/mk/build/build.mk
@@ -1,6 +1,7 @@
-# $Id: build.mk,v 1.6 2006-01-29 23:56:06 juli Exp $
+# $Id: build.mk,v 1.7 2006-01-30 00:10:11 juli Exp $
 
 .PATH: ${KERNEL_ROOT}/core
+.PATH: ${KERNEL_ROOT}/db
 .PATH: ${KERNEL_ROOT}/io/device/console
 .PATH: ${KERNEL_ROOT}/vm
 

609a726ed20ad8b3e2b5cad5a0c189d11c95b5d2
Juli Mallett <juli@redactedconsulting.com>
2006-01-30 06:28:57 +0000

    *** empty log message ***

--- mu/mk/cpu/mips/cpu/atomic.h
+++ mu/mk/cpu/mips/cpu/atomic.h
@@ -18,17 +18,32 @@ atomic_store_64(volatile uint64_t *p, uint64_t v)
 static inline bool
 atomic_compare_and_set_64(volatile uint64_t *p, uint64_t o, uint64_t v)
 {
-	if (atomic_load_64(p) == o) {
-		atomic_store_64(p, v);
-		return (true);
-	}
-	return (false);
+	uint64_t temp;
+	int res;
+
+	__asm __volatile (
+	"1:\n\t"
+	"move	%[res], $0\n\t"
+	"lld	%[temp], %[p]\n\t"
+	"bne	%[temp], %[o], 2f\n\t"
+	"move	%[temp], %[v]\n\t"
+	"li	%[res], 1\n\t"
+	"scd	%[temp], %[p]\n\t"
+	"beqz	%[temp], 1b\n\t"
+	"2:\n\t"
+	: [res] "=&r"(res), [temp] "=&r"(temp), [p] "+m"(*p)
+	: [o] "r"(o), [v] "r"(v)
+	: "memory"
+	);
+
+	return (res != 0);
 }
 
 static inline void
 atomic_decrement_64(volatile uint64_t *p)
 {
 	uint64_t o;
+
 	o = atomic_load_64(p);
 	while (!atomic_compare_and_set_64(p, o, o - 1))
 		o = atomic_load_64(p);
@@ -38,6 +53,7 @@ static inline void
 atomic_increment_64(volatile uint64_t *p)
 {
 	uint64_t o;
+
 	o = atomic_load_64(p);
 	while (!atomic_compare_and_set_64(p, o, o + 1))
 		o = atomic_load_64(p);

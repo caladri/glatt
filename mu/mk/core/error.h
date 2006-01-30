1555cf2c8386357e6868a5f425f8845503541b76
Juli Mallett <juli@redactedconsulting.com>
2006-01-30 00:27:14 +0000

    Add a not-implemented error code.

--- mu/mk/core/error.h
+++ mu/mk/core/error.h
@@ -5,5 +5,6 @@
 #define	ERROR_NOT_FOUND		(0x0001)	/* Resource not found.  */
 #define	ERROR_NOT_PERMITTED	(0x0002)	/* Operation not permitted.  */
 #define	ERROR_EXHAUSTED		(0x0003)	/* Resource exhausted.  */
+#define	ERROR_NOT_IMPLEMENTED	(0x0004)	/* Lazy programmer.  */
 
 #endif /* !_CORE_ERROR_H_ */

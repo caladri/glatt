c41c7ee2550fd2166090612eb8d37acb4f115adc
Juli Mallett <juli@redactedconsulting.com>
2006-01-30 04:04:00 +0000

    Allow use in assembly files.

--- mu/mk/cpu/mips/page/page.h
+++ mu/mk/cpu/mips/page/page.h
@@ -1,14 +1,18 @@
 #ifndef	_PAGE_PAGE_H_
 #define	_PAGE_PAGE_H_
 
+#ifndef	ASSEMBLER
 #include <page/types.h>
 
 struct vm;
+#endif
 
 #define	PAGE_SHIFT	(13)
 
+#ifndef	ASSEMBLER
 int pmap_map(struct vm *, vaddr_t, paddr_t);
 int pmap_map_direct(struct vm *, paddr_t, vaddr_t *);
 int pmap_unmap(struct vm *, vaddr_t);
+#endif
 
 #endif /* !_PAGE_PAGE_H_ */

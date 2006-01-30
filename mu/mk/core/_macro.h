bc6ecaeb20f7828a8c237d30988a5d520a36f6f8
Juli Mallett <juli@redactedconsulting.com>
2006-01-30 11:34:13 +0000

    Expand helper macros.

--- mu/mk/core/_macro.h
+++ mu/mk/core/_macro.h
@@ -1,12 +1,24 @@
 #ifndef	_CORE_MACRO_H_
 #define	_CORE_MACRO_H_
 
+	/* Handy macros.  */
+
 #define	MIN(a, b)		((a) < (b) ? (a) : (b))
 #define	MAX(a, b)		((a) > (b) ? (a) : (b))
+
+	/* Preprocessor magic.  */
 
 #define	_CONCAT(x, y)		x ## y
 #define	CONCAT(x, y)		_CONCAT(x, y)
 #define	STRING(t)		#t
+
+	/* Function, variable, etc., attributes.  */
+
+#define	__noreturn		__attribute__ ((__noreturn__))
+#define	__packed		__attribute__ ((__packed__))
+#define	__unused		__attribute__ ((__unused__))
+
+	/* Compile-time assertions.  */
 
 #ifndef	ASSEMBLER
 #define	COMPILE_TIME_ASSERT(p)						\

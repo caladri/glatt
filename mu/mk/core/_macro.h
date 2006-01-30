340fdc70e79c0375268fda5b7d4fafacc71fa51b
Juli Mallett <juli@redactedconsulting.com>
2006-01-30 01:24:41 +0000

    Add macros to concat and to do compile time assertions.

--- mu/mk/core/_macro.h
+++ mu/mk/core/_macro.h
@@ -1,6 +1,14 @@
 #ifndef	_CORE_MACRO_H_
 #define	_CORE_MACRO_H_
 
-#define	STRING(t)	#t
+#define	_CONCAT(x, y)		x ## y
+#define	CONCAT(x, y)		_CONCAT(x, y)
+#define	STRING(t)		#t
+
+#define	COMPILE_TIME_ASSERT(p)						\
+	typedef	uint8_t CONCAT(ctassert_, __LINE__) [!(p) * -1]
+
+COMPILE_TIME_ASSERT(true);
+COMPILE_TIME_ASSERT(!false);
 
 #endif /* !_CORE_MACRO_H_ */

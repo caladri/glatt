858dec17ad88d6725730c6d1222202001184b985
Juli Mallett <juli@redactedconsulting.com>
2006-01-30 10:05:44 +0000

    Add CPU interrupt enable/disable code.

--- mu/mk/cpu/mips/cpu/cpu_interrupt.c
+++ mu/mk/cpu/mips/cpu/cpu_interrupt.c
@@ -1,21 +1,25 @@
 #include <core/types.h>
+#include <core/mp.h>
 #include <cpu/cpu.h>
 #include <cpu/interrupt.h>
+#include <io/device/console/console.h>
 
 register_t
 cpu_interrupt_disable(void)
 {
-	return (cpu_read_status());
+	return (cpu_read_status() & CP0_STATUS_IE);
 }
 
 void
 cpu_interrupt_enable(void)
 {
+	kcprintf("cpu%u: interrupts enabled.\n", mp_whoami());
+	cpu_write_status(cpu_read_status() | CP0_STATUS_IE);
 }
 
 void
 cpu_interrupt_restore(register_t r)
 {
-	if (cpu_read_status() != r)
-		cpu_write_status(r);
+	if ((cpu_read_status() & CP0_STATUS_IE) != r)
+		cpu_write_status(cpu_read_status() | r);
 }

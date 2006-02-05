e8c56f95c815441f4878328b165a123f33e7bcac
Juli Mallett <juli@redactedconsulting.com>
2006-02-05 09:39:50 +0000

    pcpu_get() => pcpu_me() Add task_me()

--- mu/mk/cpu/mips/cpu/pcpu.h
+++ mu/mk/cpu/mips/cpu/pcpu.h
@@ -15,9 +15,15 @@ struct pcpu {
 };
 
 static __inline volatile struct pcpu *
-pcpu_get(void)
+pcpu_me(void)
 {
 	return ((volatile struct pcpu *)PCPU_VIRTUAL);
+}
+
+static __inline volatile struct task *
+task_me(void)
+{
+	return (pcpu_me()->pc_task);
 }
 
 #endif /* !_CPU_PCPU_H_ */

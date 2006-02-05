4bc421eb8fde7b87e054460123310b51c92efc6c
Juli Mallett <juli@redactedconsulting.com>
2006-02-05 09:05:59 +0000

    Volatile for PCPU.  XXX should use assembly and all that, maybe?

--- mu/mk/cpu/mips/cpu/pcpu.h
+++ mu/mk/cpu/mips/cpu/pcpu.h
@@ -4,17 +4,20 @@
 #include <cpu/cpuinfo.h>
 #include <cpu/memory.h>
 
+struct task;
+
 #define	PCPU_VIRTUAL	(XKSEG_BASE)
 
 	/* Per-CPU data.  */
 struct pcpu {
+	struct task *pc_task;
 	struct cpuinfo pc_cpuinfo;
 };
 
-static __inline struct pcpu *
+static __inline volatile struct pcpu *
 pcpu_get(void)
 {
-	return ((struct pcpu *)PCPU_VIRTUAL);
+	return ((volatile struct pcpu *)PCPU_VIRTUAL);
 }
 
 #endif /* !_CPU_PCPU_H_ */

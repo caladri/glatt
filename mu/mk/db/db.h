263332126c1160790b763d9c9b3f6ccc12e4717f
Juli Mallett <juli@redactedconsulting.com>
2006-02-05 09:04:49 +0000

    Assertions!

--- mu/mk/db/db.h
+++ mu/mk/db/db.h
@@ -3,6 +3,11 @@
 
 #include <cpu/db.h>
 
+#define	ASSERT(p, s)							\
+	if (!(p))							\
+		panic("%s:%u (in %s): %s failed: %s",			\
+		      __FILE__, __LINE__, __func__, #p, s)
+
 void panic(const char *, ...) __noreturn;
 
 #endif /* !_DB_DB_H_ */

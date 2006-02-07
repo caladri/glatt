d85515af90dbca5a703231d74eaaf8fcfa34a545
Juli Mallett <juli@redactedconsulting.com>
2006-02-07 09:56:25 +0000

    memcpy/memset/strlen.

--- mu/mk/core/string.h
+++ mu/mk/core/string.h
@@ -1,6 +1,36 @@
 #ifndef	_CORE_STRING_H_
 #define	_CORE_STRING_H_
 
-void memcpy(void *, const void *, size_t);
+static __inline void
+memcpy(void *dst, const void *src, size_t len)
+{
+	char *d;
+	const char *s;
+	
+	d = dst;
+	s = src;
+
+	while (len--)
+		*d++ = *s++;
+}
+
+static __inline void
+memset(void *dst, int val, size_t len)
+{
+	char *d = dst;
+
+	while (len--)
+		*d++ = val;
+}
+
+static __inline size_t
+strlen(const char *s)
+{
+	const char *p;
+
+	for (p = s; *p != '\0'; p++)
+		continue;
+	return (p - s);
+}
 
 #endif /* !_CORE_STRING_H_ */

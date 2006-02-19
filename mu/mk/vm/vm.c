806ee23d8570aab0457cd9aed4c804b311673252
Juli Mallett <juli@redactedconsulting.com>
2006-02-19 23:10:36 +0000

    Basic tree preparation.

--- mu/mk/vm/vm.c
+++ mu/mk/vm/vm.c
@@ -2,6 +2,7 @@
 #include <core/alloc.h>
 #include <core/error.h>
 #include <db/db.h>
+#include <vm/page.h>
 #include <vm/vm.h>
 
 struct vm_index {
@@ -9,10 +10,14 @@ struct vm_index {
 	size_t vmi_size;
 	struct vm_index *vmi_left;
 	struct vm_index *vmi_right;
+	struct vm_index *vmi_free_prev;
+	struct vm_index *vmi_free_next;
 };
 
 static struct pool vm_index_pool;
 struct vm kernel_vm;
+
+static void vm_insert_index(struct vm_index *, struct vm_index *);
 
 void
 vm_init(void)
@@ -40,5 +45,30 @@ vm_free_address(struct vm *vm, vaddr_t vaddr)
 int
 vm_insert_range(struct vm *vm, vaddr_t begin, vaddr_t end)
 {
-	return (ERROR_NOT_IMPLEMENTED);
+	struct vm_index *vmi, *t;
+
+	vmi = pool_allocate(&vm_index_pool);
+	if (vmi == NULL)
+		return (ERROR_EXHAUSTED);
+	vmi->vmi_base = begin;
+	vmi->vmi_size = PA_TO_PAGE(end - begin);
+	vmi->vmi_left = NULL;
+	vmi->vmi_right = NULL;
+	vmi->vmi_free_prev = NULL;
+	vmi->vmi_free_next = vm->vm_index_free;
+	if (vm->vm_index_free != NULL)
+		vm->vm_index_free->vmi_free_prev = vmi;
+	vm->vm_index_free = vmi;
+	if (vm->vm_index == NULL) {
+		vm->vm_index = vmi;
+		return (0);
+	}
+	vm_insert_index(vm->vm_index, vmi);
+	return (0);
+}
+
+static void
+vm_insert_index(struct vm_index *t, struct vm_index *vmi)
+{
+	panic("%s: too lazy to implement a tree right now!", __func__);
 }

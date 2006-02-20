7950f81d4aa29024a58058e84f63a1fc993ae5c5
Juli Mallett <juli@redactedconsulting.com>
2006-02-20 06:39:41 +0000

    Flesh out vm_alloc and vm_free.

--- mu/mk/vm/vm_alloc.c
+++ mu/mk/vm/vm_alloc.c
@@ -7,11 +7,61 @@
 int
 vm_alloc(struct vm *vm, size_t size, vaddr_t *vaddrp)
 {
-	return (ERROR_NOT_IMPLEMENTED);
+	size_t o, pages;
+	paddr_t paddr;
+	vaddr_t vaddr;
+	int error, error2;
+
+	pages = size / PAGE_SIZE;
+	if ((size % PAGE_SIZE) != 0)
+		pages++;
+	error = vm_alloc_address(vm, &vaddr, pages);
+	if (error != 0)
+		return (error);
+	for (o = 0; o < pages; o++) {
+		error = page_alloc(vm, &paddr);
+		if (error != 0) {
+			if (o != 0) {
+				panic("%s: must free mappings for failed allocation.", __func__);
+			}
+			error2 = vm_free_address(vm, vaddr);
+			if (error2 != 0) {
+				panic("%s: vm_free_address failed: %u",
+				      __func__, error);
+			}
+			return (error);
+		}
+		error = page_map(vm, vaddr + o * PAGE_SIZE, paddr);
+		if (error != 0) {
+			panic("%s: must free pages for failed mapping.", __func__);
+		}
+	}
+	*vaddrp = vaddr;
+	return (0);
 }
 
 int
 vm_free(struct vm *vm, size_t size, vaddr_t vaddr)
 {
+	size_t o, pages;
+	paddr_t paddr;
+	int error;
+
+	pages = size / PAGE_SIZE;
+	if ((size % PAGE_SIZE) != 0)
+		pages++;
+	for (o = 0; o < pages; o++) {
+		error = page_extract(vm, vaddr + o * PAGE_SIZE, &paddr);
+		if (error != 0)
+			panic("%s: failed to extract from mapping: %u",
+			      __func__, error);
+		error = page_release(vm, paddr);
+		if (error != 0)
+			panic("%s: failed to release page: %u", __func__,
+			      error);
+	}
+	error = vm_free_address(vm, vaddr);
+	if (error != 0)
+		panic("%s: failed to free address: %u", __func__, error);
 	return (ERROR_NOT_IMPLEMENTED);
 }

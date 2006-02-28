fb80f5cffd7fcfdc01898479bfcd4b3fe8d7055e
Juli Mallett <juli@redactedconsulting.com>
2006-02-28 07:00:11 +0000

    Arrange to get startup_boot() called.

--- mu/mk/core/core_startup.c
+++ mu/mk/core/core_startup.c
@@ -1,16 +1,28 @@
 #include <core/types.h>
+#include <core/spinlock.h>
 #include <core/startup.h>
 #include <db/db.h>
 #include <io/device/console/console.h>
+
+static bool startup_booted = false;
+static struct spinlock startup_spinlock = SPINLOCK_INIT("startup");
 
 void
 startup_boot(void)
 {
 	kcprintf("The system is coming up.\n");
+	startup_booted = true;
 }
 
 void
 startup_main(void)
 {
-	panic("%s: nothing to do.", __func__);
+	spinlock_lock(&startup_spinlock);
+	if (!startup_booted) {
+		startup_boot();
+		panic("%s: nothing to do.", __func__);
+	}
+	spinlock_unlock(&startup_spinlock);
+	for (;;) {
+	}
 }

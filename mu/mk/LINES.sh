7e2aaca75cbefe338e771861145c97af85c60c58
Juli Mallett <juli@redactedconsulting.com>
2006-02-28 09:10:00 +0000

    Allow passing in multiple directories.

--- mu/mk/LINES.sh
+++ mu/mk/LINES.sh
@@ -1,9 +1,13 @@
 #! /bin/sh
 
-if [ -z "$1" ]; then
-	DIR=.
-else
-	DIR=$1
-fi
+lines() {
+	find $1 -type f -name '*.[chS]' | grep -v build | grep -v framebuffer_font | xargs wc -l | sort -n | sed 's/total$/& '"$1"'/'
+}
 
-find $DIR -type f -name '*.[chS]' | grep -v build | grep -v framebuffer_font | xargs wc -l | sort -n
+if [ $# -eq 0 ]; then
+	lines .
+else
+	for arg in "$@"; do
+		lines $arg
+	done
+fi

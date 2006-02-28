3e8f7f8c63a836350c75ae686f2121c2f3ce40d3
Juli Mallett <juli@redactedconsulting.com>
2006-02-28 09:06:22 +0000

    Allow passing a directory as the first argument.

--- mu/mk/LINES.sh
+++ mu/mk/LINES.sh
@@ -1,3 +1,9 @@
 #! /bin/sh
 
-find . -type f -name '*.[chS]' | grep -v build | grep -v framebuffer_font | xargs wc -l | sort -n
+if [ -z "$1" ]; then
+	DIR=.
+else
+	DIR=$1
+fi
+
+find $DIR -type f -name '*.[chS]' | grep -v build | grep -v framebuffer_font | xargs wc -l | sort -n

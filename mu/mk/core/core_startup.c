d325e1c767051798b09ef98a83ccd670649a918c
Juli Mallett <juli@redactedconsulting.com>
2006-02-28 09:42:54 +0000

    More thread startup stuff.

--- mu/mk/core/core_startup.c
+++ mu/mk/core/core_startup.c
@@ -1,6 +1,8 @@
 #include <core/types.h>
 #include <core/spinlock.h>
 #include <core/startup.h>
+#include <core/task.h>
+#include <core/thread.h>
 #include <db/db.h>
 #include <io/device/console/console.h>
 
@@ -8,11 +10,14 @@ SET(startup_items, struct startup_item);
 
 static bool startup_booting = false;
 static struct spinlock startup_spinlock = SPINLOCK_INIT("startup");
+static struct task *main_task;
 
 void
 startup_boot(void)
 {
 	struct startup_item **itemp, *item;
+
+	startup_booting = true;
 	kcprintf("The system is coming up.\n");
 	for (itemp = SET_BEGIN(startup_items); itemp < SET_END(startup_items);
 	     itemp++) {
@@ -28,18 +33,33 @@ startup_boot(void)
 void
 startup_main(void)
 {
+	struct thread *thread;
+	bool bootstrap;
+	int error;
+
+	spinlock_lock(&startup_spinlock);
+	bootstrap = !startup_booting;
+
+	if (bootstrap) {
+		error = task_create(&main_task, NULL, "main", TASK_DEFAULT);
+		if (error != 0)
+			panic("%s: task_create failed: %u", __func__, error);
+	}
+
+	error = thread_create(&thread, main_task, "", THREAD_DEFAULT);
+	if (error != 0)
+		panic("%s: thread_create failed: %u", __func__, error);
+
+	spinlock_unlock(&startup_spinlock);
+
 	/*
 	 * XXX
-	 * Create thread for this CPU and switch to it if we aren't the
+	 * Switch to running our thread.
 	 */
-	spinlock_lock(&startup_spinlock);
-	if (!startup_booting) {
-		startup_booting = true;
-		spinlock_unlock(&startup_spinlock);
+
+	if (bootstrap)
 		startup_boot();
-	} else {
-		spinlock_unlock(&startup_spinlock);
-	}
+
 	for (;;) {
 	}
 }

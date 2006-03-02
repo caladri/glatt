664f16af781b411047e916eb548dfe24b90168ff
Juli Mallett <juli@redactedconsulting.com>
2006-03-02 19:59:18 +0000

    Add a generic startup function for the platform.  Mark platform_halt noreturn.

--- mu/mk/platform/testmips/platform/startup.h
+++ mu/mk/platform/testmips/platform/startup.h
@@ -1,7 +1,8 @@
 #ifndef _PLATFORM_STARTUP_H_
 #define	_PLATFORM_STARTUP_H_
 
-void platform_halt(void);
+void platform_halt(void) __noreturn;
 void platform_start(void);
+void platform_startup(void);
 
 #endif /* !_PLATFORM_STARTUP_H_ */

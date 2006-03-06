bbee90c923fe2982254da1f8b485a511d8bafe2c
Juli Mallett <juli@redactedconsulting.com>
2006-03-06 06:22:52 +0000

    Put error strings into an array.

--- mu/mk/core/error.h
+++ mu/mk/core/error.h
@@ -1,11 +1,22 @@
 #ifndef	_CORE_ERROR_H_
 #define	_CORE_ERROR_H_
 
-#define	ERROR_IT_S_ALRIGHT	(0x0000)	/* Sterling Void.  */
-#define	ERROR_NOT_FOUND		(0x0001)	/* Resource not found.  */
-#define	ERROR_NOT_PERMITTED	(0x0002)	/* Operation not permitted.  */
-#define	ERROR_EXHAUSTED		(0x0003)	/* Resource exhausted.  */
-#define	ERROR_NOT_IMPLEMENTED	(0x0004)	/* Lazy programmer.  */
-#define	ERROR_AGAIN		(0x0005)	/* Try again as needed.  */
+static const char *error_strings[] = {
+#define	ERROR_IT_S_ALRIGHT	(0x0000)
+	"Sterling Void.",
+#define	ERROR_NOT_FOUND		(0x0001)
+	"Resource not found.",
+#define	ERROR_NOT_PERMITTED	(0x0002)
+	"Operation not permitted.",
+#define	ERROR_EXHAUSTED		(0x0003)
+	"Resource exhausted.",
+#define	ERROR_NOT_IMPLEMENTED	(0x0004)
+	"Lazy programmer.",
+#define	ERROR_AGAIN		(0x0005)
+	"Try again as needed.",
+#define	ERROR_COUNT		(0x0006)
+};
+COMPILE_TIME_ASSERT(sizeof error_strings / sizeof error_strings[0] ==
+		    ERROR_COUNT);
 
 #endif /* !_CORE_ERROR_H_ */

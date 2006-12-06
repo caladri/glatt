4ee61c568466ad2cd4f46a68996dfa0c434fae3e
Juli Mallett <juli@redactedconsulting.com>
2006-12-06 01:54:56 +0000

    Attach a CPU directly to the root on malta.

--- mu/mk/platform/malta/platform/platform_startup.c
+++ mu/mk/platform/malta/platform/platform_startup.c
@@ -1,6 +1,9 @@
 #include <core/types.h>
 #include <core/mp.h>
 #include <core/startup.h>
+#include <db/db.h>
+#include <io/device/device.h>
+#include <io/device/driver.h>
 
 void
 platform_startup(void)
@@ -8,3 +11,18 @@ platform_startup(void)
 	cpu_startup();
 	startup_main();
 }
+
+static void
+platform_startup_cpu(void *arg)
+{
+	struct device *device;
+	struct driver *driver;
+	int error;
+
+	driver = driver_lookup("cpu");
+	error = device_create(&device, NULL, driver);
+	if (error != 0)
+		panic("%s: device_create failed: %m", __func__, error);
+}
+STARTUP_ITEM(platform_cpu, STARTUP_DRIVERS, STARTUP_FIRST,
+	     platform_startup_cpu, NULL);

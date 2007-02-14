42a866f39d6736289d011d70d7012791cd83c935
Juli Mallett <juli@redactedconsulting.com>
2007-02-14 09:24:26 +0000

    Move to a more direct system of getting symbol definitions for assembly.  It uses the compiler's inline-assembly feature (combined with compiling only to assembly) to generate a series of preprocessor defines that can be extracted trivially.
    Note that because of wrong-headed, well-intentioned limitations in GCC, we have
    to define functions for these assembly statements to occur within, which is
    non-ideal, but still better than the FreeBSD approach in every measurable way.

--- mu/mk/build/_assym.h
+++ mu/mk/build/_assym.h
@@ -1,51 +1,24 @@
-/*-
- * Copyright (c) 1999 Marcel Moolenaar
- * All rights reserved.
- *
- * Redistribution and use in source and binary forms, with or without
- * modification, are permitted provided that the following conditions
- * are met:
- * 1. Redistributions of source code must retain the above copyright
- *    notice, this list of conditions and the following disclaimer 
- *    in this position and unchanged.
- * 2. Redistributions in binary form must reproduce the above copyright
- *    notice, this list of conditions and the following disclaimer in the
- *    documentation and/or other materials provided with the distribution.
- * 3. The name of the author may not be used to endorse or promote products
- *    derived from this software without specific prior written permission.
- *
- * THIS SOFTWARE IS PROVIDED BY THE AUTHOR ``AS IS'' AND ANY EXPRESS OR
- * IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
- * OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
- * IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY DIRECT, INDIRECT,
- * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT
- * NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
- * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
- * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
- * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF
- * THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
- *
- * $FreeBSD: src/sys/sys/assym.h,v 1.5 2004/03/12 21:45:32 trhodes Exp $
- */
-
 #ifndef _BUILD__ASSYM_H_
 #define	_BUILD__ASSYM_H_
 
-#define	ASSYM_BIAS		0x10000	/* avoid zero-length arrays */
-#define	ASSYM_ABS(value)	((value) > 0 ? (value) : -((value) + 1) + 1ULL)
+/*
+ * XXX GCC is broken and won't allow constraints in global assembly, even when
+ * those constraints are, say, "n", constants which do not require any
+ * computation.  I'm astounded --juli.
+ */
 
-#define	DEFINE(name, value)						      \
-char name ## sign[((value) > 0 ? 0 : ((value) == 0 ? 0 : 1)) + ASSYM_BIAS];   \
-char name ## w0[(ASSYM_ABS(value) & 0xFFFFU) + ASSYM_BIAS];		      \
-char name ## w1[((ASSYM_ABS(value) & 0xFFFF0000UL) >> 16) + ASSYM_BIAS];      \
-char name ## w2[((ASSYM_ABS(value) & 0xFFFF00000000ULL) >> 32) + ASSYM_BIAS]; \
-char name ## w3[((ASSYM_ABS(value) & 0xFFFF000000000000ULL) >> 48) + ASSYM_BIAS]
+#define	DEFINE(name, value)						\
+void gcc_is_dumb_ ## name(void);					\
+void gcc_is_dumb_ ## name(void)						\
+{									\
+	__asm ("@assym@ define " #name " %0" : : "n"(value));		\
+} struct __hack
 
 #define	DEFINE_CONSTANT(name)						     \
-char name ## sign[((name) > 0 ? 0 : ((name) == 0 ? 0 : 1)) + ASSYM_BIAS];    \
-char name ## w0[(ASSYM_ABS(name) & 0xFFFFU) + ASSYM_BIAS];		     \
-char name ## w1[((ASSYM_ABS(name) & 0xFFFF0000UL) >> 16) + ASSYM_BIAS];      \
-char name ## w2[((ASSYM_ABS(name) & 0xFFFF00000000ULL) >> 32) + ASSYM_BIAS]; \
-char name ## w3[((ASSYM_ABS(name) & 0xFFFF000000000000ULL) >> 48) + ASSYM_BIAS]
+void gcc_is_dumb_ ## name(void);					\
+void gcc_is_dumb_ ## name(void)						\
+{									\
+	__asm ("@assym@ define " #name " %0" : : "n"(name));		\
+} struct __hack
 
 #endif /* !_BUILD__ASSYM_H_ */

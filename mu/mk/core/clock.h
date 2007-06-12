594f00eedb21c97b6e40314f389577505e955073
Juli Mallett <juli@redactedconsulting.com>
2007-06-12 02:00:16 +0000

    Less primitive interface.

--- mu/mk/core/clock.h
+++ mu/mk/core/clock.h
@@ -1,7 +1,9 @@
 #ifndef	_CORE_CLOCK_H_
 #define	_CORE_CLOCK_H_
 
-unsigned clock(void);
-void clock_ticks(unsigned);
+typedef	uint64_t	clock_tick_t;
+
+clock_tick_t clock(void);
+void clock_ticks(clock_tick_t);
 
 #endif /* !_CORE_CLOCK_H_ */

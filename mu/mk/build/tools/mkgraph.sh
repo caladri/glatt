2eff6e184d9633b68aae8ce8a1e9097fd6be3374
Juli Mallett <juli@redactedconsulting.com>
2007-06-12 01:43:40 +0000

    Tweak graphs.

--- mu/mk/build/tools/mkgraph.sh
+++ mu/mk/build/tools/mkgraph.sh
@@ -1,6 +1,8 @@
 #! /bin/sh
 
+bsdmake clean
 env KERNEL_CFLAGS=-dr bsdmake
-egypt *expand > graph/graph.dot
+egypt --include-external *expand > graph/graph.dot
 /usr/local/graphviz-2.12/bin/dot -Tpdf graph/graph.dot > graph/graph.pdf
 rm *expand
+bsdmake clean

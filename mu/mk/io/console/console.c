54f3c80fca5b497125e5bb2b3316d2293bfa611d
Juli Mallett <juli@redactedconsulting.com>
2007-06-18 06:12:35 +0000

    Put console locking in a macro.

--- mu/mk/io/console/console.c
+++ mu/mk/io/console/console.c
@@ -10,6 +10,9 @@ static void cflush(struct console *);
 static void cputc_noflush(void *, char);
 static void cputs_noflush(struct console *, const char *);
 
+#define	CONSOLE_LOCK(c)		spinlock_lock(&(c)->c_lock)
+#define	CONSOLE_UNLOCK(c)	spinlock_unlock(&(c)->c_lock)
+
 void
 console_init(struct console *console)
 {
@@ -24,9 +27,9 @@ kcgetc(char *chp)
 	char ch;
 	int error;
 
-	spinlock_lock(&kernel_console->c_lock);
+	CONSOLE_LOCK(kernel_console);
 	error = kernel_console->c_getc(kernel_console->c_softc, &ch);
-	spinlock_unlock(&kernel_console->c_lock);
+	CONSOLE_UNLOCK(kernel_console);
 	if (error != 0)
 		return (error);
 	*chp = ch;
@@ -36,19 +39,19 @@ kcgetc(char *chp)
 void
 kcputc(char ch)
 {
-	spinlock_lock(&kernel_console->c_lock);
+	CONSOLE_LOCK(kernel_console);
 	cputc_noflush(kernel_console, ch);
 	cflush(kernel_console);
-	spinlock_unlock(&kernel_console->c_lock);
+	CONSOLE_UNLOCK(kernel_console);
 }
 
 void
 kcputs(const char *s)
 {
-	spinlock_lock(&kernel_console->c_lock);
+	CONSOLE_LOCK(kernel_console);
 	cputs_noflush(kernel_console, s);
 	cflush(kernel_console);
-	spinlock_unlock(&kernel_console->c_lock);
+	CONSOLE_UNLOCK(kernel_console);
 }
 
 void
@@ -64,10 +67,10 @@ kcprintf(const char *s, ...)
 void
 kcvprintf(const char *s, va_list ap) 
 {
-	spinlock_lock(&kernel_console->c_lock);
+	CONSOLE_LOCK(kernel_console);
 	kfvprintf(cputc_noflush, kernel_console, s, ap);
 	cflush(kernel_console);
-	spinlock_unlock(&kernel_console->c_lock);
+	CONSOLE_LOCK(kernel_console);
 }
 
 static void

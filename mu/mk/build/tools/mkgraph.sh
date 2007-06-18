c56044e63a9fc60ff727bb9523d6cf0d79a9a273
Juli Mallett <juli@redactedconsulting.com>
2007-06-18 09:18:34 +0000

    Get rid of a bunch of general-use functions so we can see a more functionally-oriented graph.

--- mu/mk/build/tools/mkgraph.sh
+++ mu/mk/build/tools/mkgraph.sh
@@ -2,7 +2,7 @@
 
 bsdmake clean
 env KERNEL_CFLAGS=-dr NO_ASSERT=t bsdmake
-egypt --include-external --omit panic,spinlock_lock,spinlock_unlock *expand > graph/graph.dot
+egypt --include-external --omit cpu_barrier,malloc,free,pool_allocate,pool_free,mutex_lock,mutex_unlock,mp_whoami,kcprintf,panic,spinlock_lock,spinlock_unlock *expand > graph/graph.dot
 /usr/local/graphviz-2.12/bin/dot -Tpdf graph/graph.dot > graph/graph.pdf
 rm *expand
 bsdmake clean

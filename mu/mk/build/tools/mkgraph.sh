c71a5e2ef08b40f6887cc9c3b95034f0a8517459
Juli Mallett <juli@redactedconsulting.com>
2007-06-18 09:15:09 +0000

    Omit panic, spinlock_{,un}lock from graph.

--- mu/mk/build/tools/mkgraph.sh
+++ mu/mk/build/tools/mkgraph.sh
@@ -2,7 +2,7 @@
 
 bsdmake clean
 env KERNEL_CFLAGS=-dr NO_ASSERT=t bsdmake
-egypt --include-external *expand > graph/graph.dot
+egypt --include-external --omit panic,spinlock_lock,spinlock_unlock *expand > graph/graph.dot
 /usr/local/graphviz-2.12/bin/dot -Tpdf graph/graph.dot > graph/graph.pdf
 rm *expand
 bsdmake clean

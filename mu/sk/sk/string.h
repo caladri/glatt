fe728ae3eb0d555c0451d764ffc72b971aa437d4
Juli Mallett <juli@redactedconsulting.com>
2007-06-30 10:01:26 +0000

    Add memcpy.

--- mu/sk/sk/string.h
+++ mu/sk/sk/string.h
@@ -2,6 +2,16 @@
 #define	_SK_STRING_H_
 
 static inline void
+memcpy(void *dst, const void *src, size_t length)
+{
+	char *d = dst;
+	const char *s = src;
+
+	while (length--)
+		*d++ = *s++;
+}
+
+static inline void
 memset(void *data, int fill, size_t length)
 {
 	char *p = data;

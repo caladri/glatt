405199604e8bbc534eedfba0c47455251315ec8b
Juli Mallett <juli@redactedconsulting.com>
2008-02-20 03:35:05 +0000

    o) Regenerate graph. o) Update mkgraph.sh for non-OSX.

--- mu/mk/build/tools/mkgraph.sh
+++ mu/mk/build/tools/mkgraph.sh
@@ -1,8 +1,8 @@
 #! /bin/sh
 
-bsdmake clean
-env KERNEL_CFLAGS=-dr NO_ASSERT=t bsdmake
-egypt --include-external --omit cpu_barrier,malloc,free,pool_allocate,pool_free,mutex_lock,mutex_unlock,mp_whoami,kcprintf,panic,spinlock_lock,spinlock_unlock *expand > graph/graph.dot
-/usr/local/graphviz-2.12/bin/dot -Tpdf graph/graph.dot > graph/graph.pdf
+make clean
+env KERNEL_CFLAGS=-dr NO_ASSERT=t make
+egypt --include-external --omit cpu_barrier,malloc,free,pool_allocate,pool_free,mutex_lock,mutex_unlock,mp_whoami,kcprintf,panic,spinlock_lock,spinlock_unlock,morder_lock,morder_unlock *expand > graph/graph.dot
+dot -Tpng graph/graph.dot > graph/graph.png
 rm *expand
-bsdmake clean
+make clean

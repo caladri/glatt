599eba9495150e9c62cf91a5271ef91710771fd3
Juli Mallett <juli@redactedconsulting.com>
2008-04-02 21:34:09 +0000

    o) Don't check in call graph. o) Generate call graph without excluding any functions.

--- mu/mk/build/tools/mkgraph.sh
+++ mu/mk/build/tools/mkgraph.sh
@@ -2,7 +2,7 @@
 
 bsdmake clean
 env KERNEL_CFLAGS=-dr NO_ASSERT=t bsdmake
-egypt --include-external --omit cpu_barrier,malloc,free,pool_allocate,pool_free,mutex_lock,mutex_unlock,mp_whoami,kcprintf,panic,spinlock_lock,spinlock_unlock,morder_lock,morder_unlock *expand > graph/graph.dot
+egypt --include-external --omit morder_lock,morder_unlock *expand > graph/graph.dot
 dot -Tpng graph/graph.dot > graph/graph.png
 rm *expand
 bsdmake clean

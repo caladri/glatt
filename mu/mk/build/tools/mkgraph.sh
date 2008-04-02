6b10e73df598af51893935928e31fe712c759bec
Juli Mallett <juli@redactedconsulting.com>
2008-04-02 18:40:26 +0000

    Update graph.

--- mu/mk/build/tools/mkgraph.sh
+++ mu/mk/build/tools/mkgraph.sh
@@ -1,8 +1,8 @@
 #! /bin/sh
 
-make clean
-env KERNEL_CFLAGS=-dr NO_ASSERT=t make
+bsdmake clean
+env KERNEL_CFLAGS=-dr NO_ASSERT=t bsdmake
 egypt --include-external --omit cpu_barrier,malloc,free,pool_allocate,pool_free,mutex_lock,mutex_unlock,mp_whoami,kcprintf,panic,spinlock_lock,spinlock_unlock,morder_lock,morder_unlock *expand > graph/graph.dot
 dot -Tpng graph/graph.dot > graph/graph.png
 rm *expand
-make clean
+bsdmake clean

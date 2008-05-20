6c0dacb4256bb912c63e2998b235ab77905b5535
Juli Mallett <juli@redactedconsulting.com>
2008-05-20 06:07:52 +0000

    Actually tie disks to the build.

--- mu/mk/platform/testmips/platform.mk
+++ mu/mk/platform/testmips/platform.mk
@@ -1,10 +1,12 @@
-# $Id: platform.mk,v 1.16 2008-03-01 10:46:07 juli Exp $
+# $Id: platform.mk,v 1.17 2008-05-20 06:07:52 juli Exp $
 
 .PATH: ${PLATFORM_ROOT}/platform
 .PATH: ${PLATFORM_ROOT}/device
 
 # device
 KERNEL_SOURCES+=testmips_console.c
+KERNEL_SOURCES+=testmips_disk.c
+KERNEL_SOURCES+=testmips_diskc.c
 KERNEL_SOURCES+=testmips_ether.c
 KERNEL_SOURCES+=testmips_framebuffer.c
 KERNEL_SOURCES+=testmips_mpbus.c

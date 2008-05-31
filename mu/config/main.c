02f9c07114a244a6da6ef2c7e76aef1be3edb061
Juli Mallett <juli@redactedconsulting.com>
2008-05-31 07:33:46 +0000

    o) Add support for a CONFIG file for a platform that tells us what to do. o) Add support for files which are conditional on a configuration option.

--- mu/config/main.c
+++ mu/config/main.c
@@ -1,5 +1,8 @@
 #include <sys/types.h>
+#include <ctype.h>
 #include <err.h>
+#include <fcntl.h>
+#include <stdarg.h>
 #include <stdbool.h>
 #include <stdint.h>
 #include <stdio.h>
@@ -12,15 +15,24 @@ static const char *options_required[] = {
 	NULL
 };
 
+struct file {
+	const char *f_path;
+	struct file *f_next;
+};
+
 struct option {
 	const char *o_name;
 	bool o_enable;
+	struct file *o_files;
 	struct option *o_next;
 };
 
 static void config(struct option *, const char *);
 static void enable(struct option *, const char *, bool);
+static struct option *load(struct option *, const char *, const char *);
+static struct option *mkfile(struct option *, const char *, const char *);
 static struct option *mkoption(struct option *, const char *);
+static struct option *parse(struct option *, int, FILE *);
 static void require(struct option *, const char *);
 static struct option *search(struct option *, const char *);
 static void show(struct option *);
@@ -59,6 +71,8 @@ main(int argc, char *argv[])
 
 	configuration = *argv++;
 	argc--;
+
+	options = load(options, root, platform);
 
 	config(options, configuration);
 
@@ -126,6 +140,83 @@ enable(struct option *options, const char *name, bool yes)
 }
 
 static struct option *
+include(struct option *options, int rootdir, ...)
+{
+	const char *dir;
+	va_list ap;
+	FILE *file;
+	int rv;
+
+	rv = fchdir(rootdir);
+	if (rv == -1)
+		err(1, "could not enter kernel root directory");
+
+	va_start(ap, rootdir);
+	for (;;) {
+		dir = va_arg(ap, const char *);
+		if (dir == NULL)
+			break;
+		rv = chdir(dir);
+		if (rv == -1)
+			err(1, "could not enter subdirectory %s", dir);
+	}
+	va_end(ap);
+
+	file = fopen("CONFIG", "r");
+	if (file == NULL)
+		errx(1, "open CONFIG");
+
+	options = parse(options, rootdir, file);
+
+	fclose(file);
+
+	return (options);
+}
+
+static struct option *
+load(struct option *options, const char *root, const char *platform)
+{
+	int rootdir;
+	int srcdir;
+	int rv;
+
+	srcdir = open(".", O_RDONLY);
+	if (srcdir == -1)
+		err(1, "could not open current directory");
+
+	rootdir = open(root, O_RDONLY);
+	if (rootdir == -1)
+		err(1, "could not open kernel root directory");
+
+	options = include(options, rootdir, "platform", platform, NULL);
+	options = include(options, rootdir, "build", NULL);
+
+	rv = fchdir(srcdir);
+	if (rv == -1)
+		err(1, "could not return to last directory");
+
+	close(srcdir);
+
+	return (options);
+}
+
+static struct option *
+mkfile(struct option *options, const char *name, const char *path)
+{
+	struct option *option;
+	struct file *file;
+
+	option = search(options, name);
+	if (option == NULL)
+		return (mkfile(mkoption(options, name), name, path));
+	file = malloc(sizeof *file);
+	file->f_path = strdup(path);
+	file->f_next = option->o_files;
+	option->o_files = file;
+	return (options);
+}
+
+static struct option *
 mkoption(struct option *options, const char *name)
 {
 	struct option *option;
@@ -142,6 +233,47 @@ mkoption(struct option *options, const char *name)
 	option->o_next = options;
 
 	return (option);
+}
+
+static struct option *
+parse(struct option *options, int rootdir, FILE *file)
+{
+	char name[1024];
+	size_t offset;
+	char *line;
+	size_t len;
+
+	while ((line = fgetln(file, &len)) != NULL) {
+		if (len == 0)
+			continue;
+		line[len - 1] = '\0';
+
+		if (line[0] == '#' || line[0] == '\0')
+			continue;
+
+		offset = strcspn(line, " \t");
+		if (offset == 0)
+			errx(1, "expected option in config file.");
+		if (offset > sizeof name)
+			errx(1, "name too long: %s", line);
+		memcpy(name, line, offset);
+		name[offset] = '\0';
+		line += offset;
+
+		while (isspace(*(unsigned char *)line))
+			line++;
+
+		if (strcasecmp(name, "cpu") == 0) {
+			if (strcspn(line, " \t") != strlen(line))
+				errx(1, "whitespace at end of cpu line");
+			options = include(options, rootdir, "cpu", line, NULL);
+		} else {
+			if (strcspn(line, " \t") != strlen(line))
+				errx(1, "whitespace at end of file line");
+			options = mkfile(options, name, line);
+		}
+	}
+	return (options);
 }
 
 static void
@@ -173,8 +305,19 @@ show(struct option *options)
 	struct option *option;
 
 	printf("configuration:\n");
-	for (option = options; option != NULL; option = option->o_next)
-		printf("\t%c%s\n", option->o_enable ? '+' : '-', option->o_name);
+	for (option = options; option != NULL; option = option->o_next) {
+		printf("\t%c%s\n", option->o_enable ? '+' : '-',
+		       option->o_name);
+		if (option->o_files != NULL) {
+			struct file *file;
+
+			for (file = option->o_files; file != NULL;
+			     file = file->f_next)
+				printf("\t\t%s\n", file->f_path);
+		} else {
+			printf("\t\tno files\n");
+		}
+	}
 }
 
 static void

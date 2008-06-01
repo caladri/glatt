0b0ab6cc786cfd3049d29be74557d1c06a4d824d
Juli Mallett <juli@redactedconsulting.com>
2008-06-01 00:21:57 +0000

    o) Make configuration string part of MK_NAME. o) Put the configuration string in the build directory name. o) Only have a "make config" target.  Do building by hand.

--- mu/mk/core/copyright.h
+++ mu/mk/core/copyright.h
@@ -1,8 +1,12 @@
 #ifndef	_CORE_COPYRIGHT_H_
 #define	_CORE_COPYRIGHT_H_
 
+#ifndef CONFIGSTR
+#error "New builds must have CONFIGSTR set."
+#endif
+
 #define	MK_NAME								\
-	"Glatt MU/MK"
+	"Glatt MU/MK (" STRING(CONFIGSTR) ")"
 
 #define	COPYRIGHT							\
 	"Copyright (c) 2005-2008 The Positry.  All rights reserved."

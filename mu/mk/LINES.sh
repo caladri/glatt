728a81a5b6e8ae60b7cd412c758c05c01f0c2016
Juli Mallett <juli@redactedconsulting.com>
2008-06-07 18:54:57 +0000

    Exclude _as.h and _as.h.S

--- mu/mk/LINES.sh
+++ mu/mk/LINES.sh
@@ -4,7 +4,7 @@ sum=0
 
 lines() {
 	if [ -d $1 ]; then
-		filelist=`find $1 -type f -name '*.[chS]' | grep -v build | grep -v framebuffer_font | grep -v core/queue.h | xargs`
+		filelist=`find $1 -type f -name '*.[chS]' | grep -v build | grep -v framebuffer_font | grep -v core/queue.h | grep -v _as.h | xargs`
 		wc -l $filelist | sort -n | sed 's;total$;'"$1"' - &;'
 	else
 		wc -l $1

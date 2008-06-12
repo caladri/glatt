e3241269868ad64aad51e04260bdab0790fffeb1
Juli Mallett <juli@redactedconsulting.com>
2008-06-12 05:12:16 +0000

    Remove build/tools, which contains only outdated scripts.

--- mu/mk/build/tools/findunused.sh
+++
@@ -1,5 +0,0 @@
-#! /bin/sh
-
-mips64-gxemul-elf-nm -B -g *.o | sed 's/  */ /g' | cut -d ' ' -f 2,3 > foo.dat
-grep '^T ' foo.dat | cut -d ' ' -f 2 | sh -c 'while read sym; do grep -q ^U.$sym foo.dat || echo $sym; done'
-rm foo.dat
--- mu/mk/build/tools/mkgraph.sh
+++
@@ -1,8 +0,0 @@
-#! /bin/sh
-
-bsdmake clean
-env KERNEL_CFLAGS=-dr NO_ASSERT=t bsdmake
-egypt --include-external --omit morder_lock,morder_unlock *expand > graph/graph.dot
-dot -Tpng graph/graph.dot > graph/graph.png
-rm *expand
-bsdmake clean

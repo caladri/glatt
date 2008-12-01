d25db130d55a21b2508f29152c81c5ed767fb492
Juli Mallett <juli@redactedconsulting.com>
2008-12-01 06:19:34 +0000

    Properly handle an empty list.

--- mk/LINES.sh
+++ mk/LINES.sh
@@ -5,7 +5,9 @@ sum=0
 lines() {
 	if [ -d $1 ]; then
 		filelist=`find $1 -type f -name '*.[chS]' | grep -v build | grep -v framebuffer_font | grep -v core/queue.h | grep -v _as.h | xargs`
-		wc -l $filelist | sort -n | sed 's;total$;'"$1"' - &;'
+		if [ "${filelist}" != "" ]; then
+			wc -l $filelist | sort -n | sed 's;total$;'"$1"' - &;'
+		fi
 	else
 		wc -l $1
 	fi

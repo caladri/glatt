e18ea85c35054900dc3d6dc2cbb773a5c5ff6016
Juli Mallett <juli@redactedconsulting.com>
2008-12-20 01:43:18 +0000

    Put real ASSERT behind INVARIANTS.

--- mk/core/assert.h
+++ mk/core/assert.h
@@ -1,12 +1,25 @@
 #ifndef	_CORE_ASSERT_H_
 #define	_CORE_ASSERT_H_
 
+#ifdef INVARIANTS
 #define	ASSERT(p, s)							\
 	if (!(p))							\
 		panic("%s:%u (in %s): %s failed: %s",			\
 		      __FILE__, __LINE__, __func__, #p, s)
+#else
+#define	ASSERT(p, s)							\
+	do {								\
+		if (false) {						\
+			(void)(p);					\
+		}							\
+	} while (0)
+#endif
 
+#ifdef INVARIANTS
 #define	NOTREACHED()	ASSERT(false, "Should not be reached.");
+#else
+#define	NOTREACHED()	do { } while (true)
+#endif
 
 void panic(const char *, ...) __noreturn/*XXX %m __printf(1, 2)*/;
 

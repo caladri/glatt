158abef525d629b0d9edee0e684750d594a2413f
Juli Mallett <juli@redactedconsulting.com>
2009-11-29 08:44:31 +0000

    Parse free memory descriptors from U-Boot.

--- mk/platform/octeon/platform/platform_start.c
+++ mk/platform/octeon/platform/platform_start.c
@@ -12,7 +12,7 @@
 #include <platform/bootinfo.h>
 #include <vm/vm_page.h>
 
-extern char __bss_start[], _end[];
+extern char __bss_start[], __bss_stop[], _end[];
 
 void
 platform_halt(void)
@@ -24,14 +24,16 @@ platform_halt(void)
 void
 platform_start(int argc, char **argv, int initcore, struct octeon_boot_descriptor *boot_descriptor)
 {
+	struct octeon_boot_memory_descriptor *boot_mem_descriptor;
+	struct octeon_memory_descriptor *mem_descriptor;
 	struct octeon_boot_info *boot_info;
-	size_t membytes;
+	paddr_t mem_base, pcpu_addr;
 	int error;
 
 	/*
 	 * Initialize the BSS.
 	 */
-	memset(__bss_start, 0, _end - __bss_start);
+	memset(__bss_start, 0, __bss_stop - __bss_start);
 
 	/*
 	 * Set up the console.
@@ -52,36 +54,51 @@ platform_start(int argc, char **argv, int initcore, struct octeon_boot_descripto
 	if (boot_descriptor->size != sizeof *boot_descriptor) {
 		panic("Boot descriptor has wrong size; got %u expected %zu.", boot_descriptor->size, sizeof *boot_descriptor);
 	}
+	if (boot_descriptor->info == 0) {
+		panic("No boot info.");
+	}
 
 	/*
 	 * Extract the boot info.
 	 */
 	boot_info = (struct octeon_boot_info *)XKPHYS_MAP(CCA_CNC, boot_descriptor->info);
-	if (boot_info == NULL) {
-		panic("No boot info.");
+	if (boot_info->memory_descriptor == 0) {
+		panic("No memory descriptors.");
 	}
 
 	/*
-	 * Retrieve the amount of RAM available and steal a page for the PCPU
-	 * data.  Skip the exception vectors and where the kernel is loaded.
+	 * Extract memory for PCPU data.
 	 */
-#define	KERNEL_MAX_SIZE		(4ul * 1024 * 1024)
-#define	KERNEL_OFFSET		(1ul * 1024 * 1024)
-#define	KERNEL_PHYSICAL_HOLE	(KERNEL_MAX_SIZE + KERNEL_OFFSET)
-#define	KERNEL_PHYSICAL_BASE	(KERNEL_PHYSICAL_HOLE + PAGE_SIZE)
-	membytes = boot_info->dram_mb * 1024 * 1024;
-	if (membytes <= KERNEL_PHYSICAL_BASE)
-		panic("%s: not enough attached memory.", __func__);
-	if (KSEG_EXTRACT(_end) >= KERNEL_PHYSICAL_BASE)
-		panic("%s: kernel end is beyond physical hole (%p >= %p).",
-		      __func__, (void *)KSEG_EXTRACT(_end),
-		      (void *)KERNEL_PHYSICAL_BASE);
-	membytes -= KERNEL_PHYSICAL_BASE;
+	boot_mem_descriptor = (struct octeon_boot_memory_descriptor *)XKPHYS_MAP(CCA_CNC, boot_info->memory_descriptor);
+	if (boot_mem_descriptor->head == 0) {
+		panic("No memory descriptor head.");
+	}
+
+	mem_base = boot_mem_descriptor->head;
+	pcpu_addr = 0;
+	while (mem_base != 0) {
+		mem_descriptor = (struct octeon_memory_descriptor *)XKPHYS_MAP(CCA_CNC, mem_base);
+
+		if (mem_descriptor->size >= PAGE_SIZE) {
+			pcpu_addr = mem_base + mem_descriptor->size - PAGE_SIZE;
+			pcpu_addr = PAGE_FLOOR(pcpu_addr);
+			if (pcpu_addr >= mem_base + sizeof *mem_descriptor) {
+				mem_descriptor->size = pcpu_addr - mem_base;
+				break;
+			}
+			pcpu_addr = 0;
+		}
+
+		mem_base = mem_descriptor->next;
+	}
+	if (pcpu_addr == 0) {
+		panic("Could not steal page for PCPU data.");
+	}
 
 	/*
 	 * Set up PCPU data, etc.  We'd like to do this earlier but can't yet.
 	 */
-	cpu_startup(KERNEL_PHYSICAL_HOLE);
+	cpu_startup(pcpu_addr);
 
 	/*
 	 * Turn on exception handlers.
@@ -93,11 +110,35 @@ platform_start(int argc, char **argv, int initcore, struct octeon_boot_descripto
 	 */
 	page_init();
 
-	error = page_insert_pages(KERNEL_PHYSICAL_BASE, ADDR_TO_PAGE(membytes));
-	if (error != 0)
-		panic("page_insert_pages %lx..%lx failed: %m",
-		      ADDR_TO_PAGE(KERNEL_PHYSICAL_BASE),
-		      ADDR_TO_PAGE(membytes), error);
+	mem_base = boot_mem_descriptor->head;
+	mem_descriptor = (struct octeon_memory_descriptor *)XKPHYS_MAP(CCA_CNC, mem_base);
+	while (mem_base != 0) {
+		paddr_t phys_addr = mem_base;
+		size_t size = mem_descriptor->size;
+
+		mem_base = mem_descriptor->next;
+		mem_descriptor = (struct octeon_memory_descriptor *)XKPHYS_MAP(CCA_CNC, mem_base);
+
+		if (!PAGE_ALIGNED(phys_addr)) {
+			if (PAGE_SIZE - PAGE_OFFSET(phys_addr) >= size) {
+				kcprintf("Skipping memory descriptor too small for page alignment.\n");
+				continue;
+			}
+			size -= PAGE_SIZE - PAGE_OFFSET(phys_addr);
+			phys_addr += PAGE_SIZE - PAGE_OFFSET(phys_addr);
+		}
+
+		if (size < PAGE_SIZE) {
+			kcprintf("Skipping small memory descriptor.\n");
+			continue;
+		}
+
+		error = page_insert_pages(phys_addr, size / PAGE_SIZE);
+		if (error != 0)
+			panic("page_insert_pages %lx..%lx failed: %m",
+			      ADDR_TO_PAGE(phys_addr),
+			      ADDR_TO_PAGE(size / PAGE_SIZE), error);
+	}
 
 	/*
 	 * Start everything that needs done before threading.

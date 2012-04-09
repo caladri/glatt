ebdf0f8e67b7a07de8e9269c4d46e0e8ec7c98c0
Juli Mallett <juli@redactedconsulting.com>
2012-04-09 03:25:55 +0000

    Shrink some fields.  Make provisions for splitting our replies and errors in the message itself.

--- mk/ipc/ipc.h
+++ mk/ipc/ipc.h
@@ -6,8 +6,8 @@
 #endif
 
 typedef	uint32_t	ipc_port_t;
-typedef	int32_t		ipc_msg_t;
-typedef	uint32_t	ipc_port_right_t;
+typedef	uint16_t	ipc_msg_t;
+typedef	uint16_t	ipc_port_right_t;
 typedef	uint64_t	ipc_cookie_t;
 typedef	uint16_t	ipc_size_t;
 
@@ -15,10 +15,10 @@ typedef	uint16_t	ipc_size_t;
 #define	IPC_PORT_NS			(1)
 #define	IPC_PORT_UNRESERVED_START	(2)
 
-#define	IPC_PORT_RIGHT_NONE		(0x00000000)
-#define	IPC_PORT_RIGHT_SEND		(0x00000001)
-#define	IPC_PORT_RIGHT_SEND_ONCE	(0x00000002)
-#define	IPC_PORT_RIGHT_RECEIVE		(0x00000004)
+#define	IPC_PORT_RIGHT_NONE		(0x0000)
+#define	IPC_PORT_RIGHT_SEND		(0x0001)
+#define	IPC_PORT_RIGHT_SEND_ONCE	(0x0002)
+#define	IPC_PORT_RIGHT_RECEIVE		(0x0004)
 
 /*
  * The minimum fields a message contains.
@@ -28,19 +28,43 @@ struct ipc_header {
 	ipc_port_t ipchdr_dst;
 	ipc_port_right_t ipchdr_right;	/* Right to give dst on src.  */
 	ipc_msg_t ipchdr_msg;		/* Opaque to IPC code, except IPC_MSG_NONE.  */
-	ipc_cookie_t ipchdr_cookie;	/* Opaque to IPC code.  */
 	ipc_size_t ipchdr_recsize;	/* Opaque to IPC code.  */
 	ipc_size_t ipchdr_reccnt;	/* Opaque to IPC code.  */
+	ipc_cookie_t ipchdr_cookie;	/* Opaque to IPC code.  */
 };
 
 #define	IPC_MSG_NONE		(0)	/* Requires no right to send, may not have data.  */
-#define	IPC_MSG_REPLY(msg)	(-(msg))
+
+/*
+ * These are conventional but not semantic to the IPC code.
+ */
+#define	IPC_MSG_FLAG_MASK	(0xf000)
+#define	IPC_MSG_MASK		(0x0fff)
+
+#define	IPC_MSG_FLAG_REQUEST	(0x0000)
+#define	IPC_MSG_FLAG_REPLY	(0x1000)
+#define	IPC_MSG_FLAG_ERROR	(0x2000)
+
+#define	IPC_MSG_REQUEST(msg)	(IPC_MSG_FLAG_REQUEST | ((msg) & IPC_MSG_MASK))
+#define	IPC_MSG_REPLY(msg)	(IPC_MSG_FLAG_REPLY | ((msg) & IPC_MSG_MASK))
+#define	IPC_MSG_ERROR(msg)	(IPC_MSG_FLAG_ERROR | ((msg) & IPC_MSG_MASK))
 
 #define	IPC_HEADER_REPLY(ipchdrp)					\
 	((struct ipc_header) {						\
 		.ipchdr_src = (ipchdrp)->ipchdr_dst,			\
 		.ipchdr_dst = (ipchdrp)->ipchdr_src,			\
 		.ipchdr_msg = IPC_MSG_REPLY((ipchdrp)->ipchdr_msg),	\
+		.ipchdr_right = IPC_PORT_RIGHT_NONE,			\
+	 	.ipchdr_cookie = (ipchdrp)->ipchdr_cookie,		\
+	 	.ipchdr_recsize = 0,					\
+	 	.ipchdr_reccnt = 0,					\
+	})
+
+#define	IPC_HEADER_ERROR(ipchdrp)					\
+	((struct ipc_header) {						\
+		.ipchdr_src = (ipchdrp)->ipchdr_dst,			\
+		.ipchdr_dst = (ipchdrp)->ipchdr_src,			\
+		.ipchdr_msg = IPC_MSG_ERROR((ipchdrp)->ipchdr_msg),	\
 		.ipchdr_right = IPC_PORT_RIGHT_NONE,			\
 	 	.ipchdr_cookie = (ipchdrp)->ipchdr_cookie,		\
 	 	.ipchdr_recsize = 0,					\

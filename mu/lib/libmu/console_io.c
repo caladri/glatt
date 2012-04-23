a8bca19d4f7325ffe78d797ff1dbc7473455f7f7
Juli Mallett <juli@redactedconsulting.com>
2012-04-23 17:04:06 +0000

    Split out console I/O.

---
+++ mu/lib/libmu/console_io.c
@@ -0,0 +1,80 @@
+#include <core/types.h>
+#include <core/error.h>
+#include <core/printf.h>
+#include <core/string.h>
+#include <ipc/ipc.h>
+#include <ns/ns.h>
+#include <vm/vm_page.h>
+
+#include <libmu/common.h>
+
+static void printf_putc(void *, char);
+static void printf_puts(void *, const char *, size_t);
+
+void
+puts(const char *s)
+{
+	putsn(s, strlen(s));
+}
+
+int
+getline(char *buf, size_t len)
+{
+	unsigned i;
+	int ch;
+
+	for (i = 0; i < len; i++) {
+again:		ch = getchar();
+		if (ch == -1)
+			return (ERROR_UNEXPECTED);
+		if (ch == '\010') {
+			if (i != 0) {
+				putchar(ch);
+				putchar(' ');
+				putchar(ch);
+				i--;
+			}
+			goto again;
+		}
+		putchar(ch); /* Echo back.  */
+		if (ch == '\n') {
+			buf[i] = '\0';
+			return (0);
+		}
+		buf[i] = ch;
+	}
+
+	return (ERROR_FULL);
+}
+
+void
+printf(const char *fmt, ...)
+{
+	va_list ap;
+
+	va_start(ap, fmt);
+	vprintf(fmt, ap);
+	va_end(ap);
+}
+
+void
+vprintf(const char *fmt, va_list ap)
+{
+	kfvprintf(printf_putc, printf_puts, NULL, fmt, ap);
+}
+
+
+static void
+printf_putc(void *arg, char ch)
+{
+	(void)arg;
+	putchar(ch);
+}
+
+static void
+printf_puts(void *arg, const char *s, size_t len)
+{
+	(void)arg;
+
+	putsn(s, len);
+}
--- mu/lib/libmu/util.c
+++ mu/lib/libmu/util.c
@@ -10,9 +10,6 @@
 
 static int debug = 0;
 
-static void printf_putc(void *, char);
-static void printf_puts(void *, const char *, size_t);
-
 extern void main(void);
 
 void
@@ -27,58 +24,6 @@ fatal(const char *msg, int error)
 {
 	printf("fatal: %s: %m\n", msg, error);
 	exit();
-}
-
-void
-puts(const char *s)
-{
-	putsn(s, strlen(s));
-}
-
-int
-getline(char *buf, size_t len)
-{
-	unsigned i;
-	int ch;
-
-	for (i = 0; i < len; i++) {
-again:		ch = getchar();
-		if (ch == -1)
-			return (ERROR_UNEXPECTED);
-		if (ch == '\010') {
-			if (i != 0) {
-				putchar(ch);
-				putchar(' ');
-				putchar(ch);
-				i--;
-			}
-			goto again;
-		}
-		putchar(ch); /* Echo back.  */
-		if (ch == '\n') {
-			buf[i] = '\0';
-			return (0);
-		}
-		buf[i] = ch;
-	}
-
-	return (ERROR_FULL);
-}
-
-void
-printf(const char *fmt, ...)
-{
-	va_list ap;
-
-	va_start(ap, fmt);
-	vprintf(fmt, ap);
-	va_end(ap);
-}
-
-void
-vprintf(const char *fmt, va_list ap)
-{
-	kfvprintf(printf_putc, printf_puts, NULL, fmt, ap);
 }
 
 void
@@ -238,19 +183,4 @@ free(void *ptr)
 	error = vm_page_free(page);
 	if (error != 0)
 		fatal("page free failed", error);
-}
-
-static void
-printf_putc(void *arg, char ch)
-{
-	(void)arg;
-	putchar(ch);
-}
-
-static void
-printf_puts(void *arg, const char *s, size_t len)
-{
-	(void)arg;
-
-	putsn(s, len);
 }

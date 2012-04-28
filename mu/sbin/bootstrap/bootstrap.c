fb4221fb23e3a8bdc902c8ee08d02e2f40083e25
Juli Mallett <juli@redactedconsulting.com>
2012-04-28 06:45:15 +0000

    The wrong version of mu-shell.c was copied to bootstrap.c.  Resurrect r1428.

--- mu/sbin/bootstrap/bootstrap.c
+++ mu/sbin/bootstrap/bootstrap.c
@@ -10,10 +10,12 @@
 #include <libmu/common.h>
 
 void
-main(void)
+bootstrap_main(void)
 {
 	ipc_port_t fs, file;
+	const char *argv[16];
 	char buf[1024];
+	unsigned argc;
 	size_t len;
 	int error;
 	void *p;
@@ -25,35 +27,47 @@ main(void)
 
 	for (;;) {
 		printf("# ");
-		error = getline(buf, sizeof buf);
+
+		error = getargs(buf, sizeof buf, &argc, argv, 16, " ");
 		if (error != 0)
-			fatal("getline failed", error);
+			fatal("getargs failed", error);
 
-		if (buf[0] == '\0')
+		if (argc == 0)
 			continue;
 
-		error = open(fs, buf, &file);
+		if (argc == 1) {
+			printf("usage: [hexdump | exec] [file]\n");
+			continue;
+		}
+
+		error = open(fs, argv[1], &file);
 		if (error != 0) {
-			printf("Could not open %s: %m\n", buf, error);
+			printf("Could not open %s: %m\n", argv[1], error);
 			continue;
 		}
 
-#if 0
-		error = exec(file);
-		if (error != 0) {
-			printf("Could not exec %s: %m\n", buf, error);
-			continue;
+		if (strcmp(argv[0], "hexdump") == 0) {
+			/*
+			 * XXX
+			 * Dump the whole thing.
+			 */
+			len = PAGE_SIZE;
+			error = read(file, &p, 0, &len);
+			if (error != 0) {
+				printf("Could not read %s: %m\n", buf, error);
+				continue;
+			}
+			hexdump(p, len);
+			vm_page_free(p);
+		} else if (strcmp(argv[0], "exec") == 0) {
+			error = exec(file, NULL, argc - 1, argv + 1);
+			if (error != 0) {
+				printf("Could not exec %s: %m\n", buf, error);
+				continue;
+			}
+		} else {
+			printf("usage: [hexdump | exec] [file]\n");
 		}
-#endif
-
-		len = PAGE_SIZE;
-		error = read(file, &p, 0, &len);
-		if (error != 0) {
-			printf("Could not read %s: %m\n", buf, error);
-			continue;
-		}
-		hexdump(p, len);
-		vm_page_free(p);
 
 		error = close(file);
 		if (error != 0)

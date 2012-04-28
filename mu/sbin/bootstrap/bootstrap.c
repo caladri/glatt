e13f4d0de43450518890c230ef7275e0ad1c4054
Juli Mallett <juli@redactedconsulting.com>
2012-04-28 06:45:28 +0000

    Display motd at startup.

--- mu/sbin/bootstrap/bootstrap.c
+++ mu/sbin/bootstrap/bootstrap.c
@@ -9,6 +9,8 @@
 
 #include <libmu/common.h>
 
+static void display(ipc_port_t, const char *);
+
 void
 bootstrap_main(void)
 {
@@ -24,6 +26,8 @@ bootstrap_main(void)
 
 	while ((fs = ns_lookup("ufs0")) == IPC_PORT_UNKNOWN)
 		continue;
+
+	display(fs, "/etc/motd");
 
 	for (;;) {
 		printf("# ");
@@ -72,5 +76,42 @@ bootstrap_main(void)
 		error = close(file);
 		if (error != 0)
 			fatal("close failed", error);
+	}
+}
+
+static void
+display(ipc_port_t fs, const char *path)
+{
+	ipc_port_t file;
+	off_t offset;
+	size_t len;
+	void *page;
+	int error;
+
+	error = open(fs, path, &file);
+	if (error != 0) {
+		printf("%s: open failed: %m\n", __func__, error);
+		return;
+	}
+
+	offset = 0;
+	for (;;) {
+		len = PAGE_SIZE;
+		error = read(file, &page, offset, &len);
+		if (error != 0) {
+			printf("%s: read failed: %m\n", __func__, error);
+			return;
+		}
+		if (len == 0)
+			break;
+
+		putsn(page, len);
+		offset += len;
+	}
+
+	error = close(file);
+	if (error != 0) {
+		printf("%s: close failed: %m\n", __func__, error);
+		return;
 	}
 }

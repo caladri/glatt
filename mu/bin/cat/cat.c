ed21e178834f721f04aebd1bd84079c4083d83b3
Juli Mallett <juli@redactedconsulting.com>
2012-04-29 06:29:31 +0000

    Add an /etc/rc script.

---
+++ mu/bin/cat/cat.c
@@ -0,0 +1,56 @@
+#include <core/types.h>
+#include <core/error.h>
+#include <core/printf.h>
+#include <core/string.h>
+#include <fs/fs.h>
+#include <ipc/ipc.h>
+#include <ns/ns.h>
+#include <vm/vm_page.h>
+
+#include <libmu/common.h>
+
+void
+main(int argc, char *argv[])
+{
+	ipc_port_t fs;
+	int error;
+
+	while ((fs = ns_lookup("ufs0")) == IPC_PORT_UNKNOWN)
+		continue;
+
+	while (--argc) {
+		const char *path = *++argv;
+		ipc_port_t file;
+		off_t offset;
+		size_t len;
+		void *page;
+
+		error = open(fs, path, &file);
+		if (error != 0) {
+			printf("Unable to open %s: %m\n", path, error);
+			continue;
+		}
+
+		offset = 0;
+		for (;;) {
+			len = PAGE_SIZE;
+			error = read(file, &page, offset, &len);
+			if (error != 0) {
+				printf("%s: read failed: %m\n", __func__, error);
+				break;
+			}
+
+			if (len == 0)
+				break;
+			putsn(page, len);
+			offset += len;
+			vm_page_free(page);
+		}
+
+		error = close(file);
+		if (error != 0) {
+			printf("%s: close failed: %m\n", __func__, error);
+			continue;
+		}
+	}
+}
--- mu/sbin/bootstrap/bootstrap.c
+++ mu/sbin/bootstrap/bootstrap.c
@@ -9,93 +9,31 @@
 
 #include <libmu/common.h>
 
-static void display(ipc_port_t, const char *);
+static int process_rc_line(ipc_port_t, char *);
 
 void
 bootstrap_main(void)
 {
 	ipc_port_t fs, file;
-	const char *argv[16];
-	char buf[1024];
-	unsigned argc;
-	size_t len;
+	off_t offset;
+	void *page;
 	int error;
-	void *p;
 
-	printf("MU Experimental Shell.\n");
+	printf("The system is bootstrapping.\n");
 
 	while ((fs = ns_lookup("ufs0")) == IPC_PORT_UNKNOWN)
 		continue;
 
-	display(fs, "/etc/motd");
-
-	for (;;) {
-		printf("# ");
-
-		error = getargs(buf, sizeof buf, &argc, argv, 16, " ");
-		if (error != 0)
-			fatal("getargs failed", error);
-
-		if (argc == 0)
-			continue;
-
-		if (argc == 1) {
-			printf("usage: [hexdump | exec] [file]\n");
-			continue;
-		}
-
-		error = open(fs, argv[1], &file);
-		if (error != 0) {
-			printf("Could not open %s: %m\n", argv[1], error);
-			continue;
-		}
-
-		if (strcmp(argv[0], "hexdump") == 0) {
-			/*
-			 * XXX
-			 * Dump the whole thing.
-			 */
-			len = PAGE_SIZE;
-			error = read(file, &p, 0, &len);
-			if (error != 0) {
-				printf("Could not read %s: %m\n", buf, error);
-				continue;
-			}
-			hexdump(p, len);
-			vm_page_free(p);
-		} else if (strcmp(argv[0], "exec") == 0) {
-			error = exec(file, NULL, argc - 1, argv + 1);
-			if (error != 0) {
-				printf("Could not exec %s: %m\n", buf, error);
-				continue;
-			}
-		} else {
-			printf("usage: [hexdump | exec] [file]\n");
-		}
-
-		error = close(file);
-		if (error != 0)
-			fatal("close failed", error);
-	}
-}
-
-static void
-display(ipc_port_t fs, const char *path)
-{
-	ipc_port_t file;
-	off_t offset;
-	size_t len;
-	void *page;
-	int error;
-
-	error = open(fs, path, &file);
-	if (error != 0) {
-		printf("%s: open failed: %m\n", __func__, error);
-		return;
-	}
+	error = open(fs, "/etc/rc", &file);
+	if (error != 0)
+		fatal("open /etc/rc failed", error);
 
 	offset = 0;
 	for (;;) {
+		unsigned i;
+		size_t len;
+		char *buf;
+
 		len = PAGE_SIZE;
 		error = read(file, &page, offset, &len);
 		if (error != 0) {
@@ -105,13 +43,67 @@ display(ipc_port_t fs, const char *path)
 		if (len == 0)
 			break;
 
-		putsn(page, len);
-		offset += len;
+		buf = page;
+		for (i = 0; i < len; i++) {
+			if (buf[i] == '\0')
+				fatal("/etc/rc contains embedded NUL", ERROR_UNEXPECTED);
+			if (buf[i] != '\n')
+				continue;
+			buf[i] = '\0';
+			break;
+		}
+		if (i == len)
+			fatal("line longer than a page", ERROR_UNEXPECTED);
+
+		error = process_rc_line(fs, buf);
+		if (error != 0)
+			fatal("exec /etc/rc failed", error);
+
+		offset += i + 1;
+		vm_page_free(page);
 	}
 
 	error = close(file);
-	if (error != 0) {
-		printf("%s: close failed: %m\n", __func__, error);
-		return;
-	}
+	if (error != 0)
+		fatal("close failed", error);
+
+	printf("Bootstrap exiting.\n");
 }
+
+static int
+process_rc_line(ipc_port_t fs, char *line)
+{
+	const char *argv[16];
+	ipc_port_t file;
+	unsigned argc;
+	int error;
+
+	if (line[0] == '\0' || line[0] == '#')
+		return (0);
+
+	error = splitargs(line, &argc, argv, 16, " ");
+	if (error != 0)
+		fatal("splitargs failed", error);
+
+	error = open(fs, argv[0], &file);
+	if (error != 0) {
+		printf("%s: unable to open %s: %m\n", __func__, argv[0], error);
+		return (error);
+	}
+
+	/*
+	 * XXX wait?
+	 */
+	error = exec(file, NULL, argc, argv);
+	if (error != 0) {
+		printf("%s: exec of %s failed: %m\n", __func__, argv[0], error);
+		return (error);
+	}
+	
+	error = close(file);
+	if (error != 0)
+		fatal("close failed", error);
+
+	return (0);
+}
+

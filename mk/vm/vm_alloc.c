c63838427a15ab15fd46f55a54fe2da629a6f184
Juli Mallett <juli@redactedconsulting.com>
2012-04-29 04:12:35 +0000

    Only require pool usage for kernel vm.

--- mk/vm/vm_alloc.c
+++ mk/vm/vm_alloc.c
@@ -13,7 +13,7 @@ vm_alloc(struct vm *vm, size_t size, vaddr_t *vaddrp)
 	vaddr_t vaddr;
 	int error;
 
-	if (size < pool_max_alloc)
+	if (vm == &kernel_vm && size < pool_max_alloc)
 		panic("%s: allocation too small, use pool instead.", __func__);
 
 	pages = PAGE_COUNT(size);

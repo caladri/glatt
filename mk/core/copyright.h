73622fde6edd71ca2d1a1a97a1ba8baa7c733eca
Juli Mallett <juli@redactedconsulting.com>
2013-11-18 07:43:30 +0000

    Bump year since I've done some significant stuff this year.

--- mk/core/copyright.h
+++ mk/core/copyright.h
@@ -12,7 +12,7 @@
 	(STRING(CONFIGSTR))
 
 #define	MK_COPYRIGHT							\
-	"Copyright (c) 2005-2012 Juli Mallett.  All rights reserved."
+	"Copyright (c) 2005-2013 Juli Mallett.  All rights reserved."
 
 #define	MK_VERSION							\
 	"Kailua"

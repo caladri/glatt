268231d5cb4b07590ec2f7c51a8a5d055e06dc3e
Juli Mallett <juli@clockworksquid.com>
2019-05-19 19:46:47 -0700

    Remove mailbox (I think not for the first time.)  It can be added when it functions.

--- mk/cpu/mips/cpu/cpu_thread.c
+++ mk/cpu/mips/cpu/cpu_thread.c
@@ -28,12 +28,6 @@ cpu_thread_free(struct thread *td)
 	error = vm_free(&kernel_vm, KSTACK_SIZE, td->td_kstack);
 	if (error != 0)
 		panic("%s: vm_free of kstack failed: %m", __func__, error);
-
-	if ((td->td_task->t_flags & TASK_KERNEL) == 0) {
-		error = vm_free(td->td_task->t_vm, MAILBOX_SIZE, td->td_cputhread.td_mbox);
-		if (error != 0)
-			panic("%s: vm_free of mboxfailed: %m", __func__, error);
-	}
 }
 
 void
@@ -59,7 +53,7 @@ cpu_thread_set_upcall_user(struct thread *td, vaddr_t function, register_t arg)
 int
 cpu_thread_setup(struct thread *td)
 {
-	vaddr_t kstack, mbox, ustack;
+	vaddr_t kstack, ustack;
 	unsigned off;
 	int error;
 
@@ -76,16 +70,6 @@ cpu_thread_setup(struct thread *td)
 		tlb_wired_wire(&td->td_cputhread.td_tlbwired, kernel_vm.vm_pmap,
 			       kstack + off);
 	memset(&td->td_context, 0, sizeof td->td_context);
-
-	if ((td->td_task->t_flags & TASK_KERNEL) == 0) {
-		error = vm_alloc(td->td_task->t_vm, MAILBOX_SIZE, &mbox, VM_ALLOC_DEFAULT);
-		if (error != 0)
-			return (error);
-		td->td_cputhread.td_mbox = mbox;
-		for (off = 0; off < MAILBOX_SIZE; off += MAILBOX_SIZE)
-			tlb_wired_wire(&td->td_cputhread.td_tlbwired,
-				       td->td_task->t_vm->vm_pmap, mbox + off);
-	}
 
 	if ((td->td_flags & THREAD_USTACK) != 0) {
 		error = vm_alloc_address(td->td_task->t_vm, &ustack, USTACK_SIZE / PAGE_SIZE, true);
--- mk/cpu/mips/cpu/thread.h
+++ mk/cpu/mips/cpu/thread.h
@@ -7,14 +7,12 @@ struct thread;
 
 #define	KSTACK_SIZE	(2 * PAGE_SIZE)
 #define	USTACK_SIZE	(128 * PAGE_SIZE)
-#define	MAILBOX_SIZE	(1 * PAGE_SIZE)
 
 #define	current_thread()	PCPU_GET(thread)
 
 struct cpu_thread {
 	struct frame *td_frame;
 	struct tlb_wired_context td_tlbwired;
-	vaddr_t td_mbox;
 };
 
 void cpu_thread_activate(struct thread *);

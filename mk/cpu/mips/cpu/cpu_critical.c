0d1afdae299114af7250f84d1f1deba910a9d5e7
Juli Mallett <juli@clockworksquid.com>
2019-05-19 21:53:38 -0700

    Close races around migrating when entering or leaving a critical section.

--- mk/cpu/mips/cpu/cpu_critical.c
+++ mk/cpu/mips/cpu/cpu_critical.c
@@ -7,23 +7,31 @@
 void
 cpu_critical_enter(void)
 {
+	register_t s;
+
 	ASSERT(!startup_early,
 	       "Cannot enter critical section during early startup.");
 
+	s = cpu_interrupt_disable();
+
 	if (PCPU_GET(critical_count) == 0)
-		PCPU_SET(critical_section, cpu_interrupt_disable());
+		PCPU_SET(critical_section, s);
 	PCPU_SET(critical_count, PCPU_GET(critical_count) + 1);
 }
 
 void
 cpu_critical_exit(void)
 {
+	register_t s;
+
 	ASSERT(!startup_early,
 	       "Cannot exit critical section during early startup.");
 
+	s = PCPU_GET(critical_section);
+
 	PCPU_SET(critical_count, PCPU_GET(critical_count) - 1);
 	if (PCPU_GET(critical_count) == 0)
-		cpu_interrupt_restore(PCPU_GET(critical_section));
+		cpu_interrupt_restore(s);
 }
 
 bool
